b2b8571c7713803c9a4fd05a53336218d94bafe2
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-02-27 22:56:07 +0100

    make the first three div in home components responsive

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,24 +3,33 @@ import title from "../Assets/title.svg"
 import logo from "../Assets/logo.svg"
 
 
+
 const Home = () => {
     return (
-        <React.Fragment className="d-flex flex-column">
+        <React.Fragment className="container">
             <div className="d-flex justify-content-center  ">
-                <div className="title">
-                    <img src={title} alt="title" />
+                <div className="imgTitle">
+                    <img src={title} alt="title" className="title" />
                 </div>
             </div>
             <div className="d-flex justify-content-center  ">
-                <div className="logo">
-                    <img src={logo} alt="logo" />
+                <div className="imgLogo" >
+                    <img src={logo} alt="logo" className="logo" />
                 </div>
             </div>
             <div className="d-flex justify-content-center  ">
-            <div >
-                <p className="para">For each Gategory you will have 30 Questions</p>
+                <div className="paraDiv">
+                    <p className="para">For each Gategory you will have 30 Questions</p>
+                </div>
             </div>
-        </div>
+            <div className="grid">
+                <div className="box" >
+                    <p className="boxPara">Computer Science</p>
+                </div>
+                <div className="boxTwo" >
+                    <p className="boxPara">Mathematics</p>
+                </div>
+            </div>
         </React.Fragment>
 
     )
